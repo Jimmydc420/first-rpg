2361dea647dec5d166cfe40623300e332cff06a3
jimmydc420 <jimmydc420@gmail.com>
2020-08-28 21:57:15 -0500

    CHANGE!!!

--- Character_Sheet.py
+++ Character_Sheet.py
@@ -5,6 +5,7 @@ class Character:
     stats = None
     race = None
     character_class = None
+    hit_dice = None
     line = '________________________________________'
 
     def __init__(self):
@@ -17,7 +18,9 @@ class Character:
         self.get_race()
         self.get_class()
         self.ability_score()
-        self.adjust_ability_score()
+        self.race_stat_mod()
+        self.print_stats()
+        self.class_stat_mod()
         self.print_stats()
 
     @staticmethod
@@ -44,7 +47,7 @@ class Character:
             else:
                 continue
 
-    def adjust_ability_score(self):
+    def race_stat_mod(self):
         if self.race == 'Dwarf':
             self.stats['con'] += 2
             self.stats['wis'] -= 2
@@ -63,6 +66,52 @@ class Character:
             self.stats['cha'] -= 2
             if self.stats['int'] < 3:
                 self.stats['int'] = 3
+
+    def class_stat_mod(self):
+        if self.character_class == 'Barbarian':
+            self.stats['str'] += 2
+            self.stats['dex'] += 1
+            self.stats['wis'] += 1
+            self.stats['con'] += 1
+        if self.character_class == 'Bard':
+            self.stats['cha'] += 2
+            self.stats['dex'] += 2
+            self.stats['int'] += 1
+        if self.character_class == 'Cleric':
+            self.stats['wis'] += 3
+            self.stats['con'] += 1
+            self.stats['cha'] += 1
+        if self.character_class == 'Druid':
+            self.stats['wis'] += 4
+            self.stats['dex'] += 1
+        if self.character_class == 'Fighter':
+            self.stats['str'] += 2
+            self.stats['con'] += 2
+            self.stats['dex'] += 1
+        if self.character_class == 'Monk':
+            self.stats['wis'] += 2
+            self.stats['dex'] += 2
+            self.stats['str'] += 1
+        if self.character_class == 'Paladin':
+            self.stats['cha'] += 2
+            self.stats['str'] += 2
+            self.stats['wis'] += 1
+        if self.character_class == 'Ranger':
+            self.stats['dex'] += 2
+            self.stats['wis'] += 2
+            self.stats['str'] += 1
+        if self.character_class == 'Rogue':
+            self.stats['dex'] += 2
+            self.stats['int'] += 2
+            self.stats['wis'] += 1
+        if self.character_class == 'Sorcerer':
+            self.stats['cha'] += 3
+            self.stats['dex'] += 1
+            self.stats['con'] += 1
+        if self.character_class == 'Wizard':
+            self.stats['int'] += 3
+            self.stats['dex'] += 1
+            self.stats['con'] += 1
 
     def print_stats(self):
         print(self.line)
@@ -102,6 +151,3 @@ class Character:
             else:
                 print('You have chosen', self.character_class)
                 return self.character_class
-
-
-player = Character()
